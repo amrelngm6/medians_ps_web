a8c17476cc061c8813a99ab66d028815ace3deee
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-01 09:33:18 +0200

    ...

--- send.php
+++ send.php
@@ -1,6 +1,12 @@
 <?php 
 if (isset($_POST['name']))
 {
-    mail('info@medianssolutions.com', 'New message', json_encode($_POST));
+    try {
+
+        mail('info@medianssolutions.com', 'New message', json_encode($_POST));
+    
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
 }
 ?>
