e19f564ae326af442c6b31588a3c87432abac038
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-08 14:30:21 +0200

    .

--- index.php
+++ index.php
@@ -142,7 +142,7 @@
         <div class="scrolling-content js-scrolling-content full-size pos-a-t-l what-content" data-state="what">
             <div class="scrolling-content-wrap" style="transform: translate3d(0px, 0px, 0px);">
                 <div class="flex-vert-center masterhead js-masterhead js-full-height" data-active="on" style="height: 600px;">
-                    <p class="letter-split yellow-text mb-0" aria-labelledby="Start Now."><span class="char1" aria-hidden="true">St</span><span class="char2" aria-hidden="true">art</span><span class="char3" aria-hidden="true"> N</span><span class="char4" aria-hidden="true">o</span><span class="char5" aria-hidden="true">w.</span></p>
+                    <p class="letter-split yellow-text mb-0" aria-label="Start Now."><span class="char1" aria-hidden="true">St</span><span class="char2" aria-hidden="true">art</span><span class="char3" aria-hidden="true"> N</span><span class="char4" aria-hidden="true">o</span><span class="char5" aria-hidden="true">w.</span></p>
                     <h1>MEDIANS PS Cloud management system</h1>
                 </div>
                 <section class="js-monitor-scroll" data-active="off" id="medians-intro">
