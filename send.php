22157427b153a9b47fd8fc46152aed711caf4cb4
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-09 02:47:37 +0200

    .

--- send.php
+++ send.php
@@ -2,10 +2,10 @@
     try {
 
         $data = json_encode($_POST);
-        
+
         file_put_contents('assets/form.txt', file_get_contents('assets/form.txt')."\r\n".$data);
 
-        !empty($_POST) ? mail('info@medianssolutions.com', 'New message', $data : '';
+        !empty($_POST) ? mail('info@medianssolutions.com', 'New message', $data) : '';
     
     } catch (Exception $e) {
         echo $e->getMessage();
