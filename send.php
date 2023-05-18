404a2a9b2c76d83d745d5800938af402ccea17dc
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-18 02:49:50 +0200

    .

--- send.php
+++ send.php
@@ -12,7 +12,7 @@
         
         $data = json_encode($_POST, JSON_UNESCAPED_UNICODE);
 
-        file_put_contents('assets/form.txt', file_get_contents('assets/form.txt')."\r\n".$data);
+        // file_put_contents('assets/form.txt', file_get_contents('assets/form.txt')."\r\n".$data);
 
         !empty($_POST) ? mail('info@medianssolutions.com', 'New message', $data) : '';
         echo json_encode(['result'=>'شكرا لك, سيتم التواصل معك بأقرب وقت']);
